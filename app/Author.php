235154a98a930eecf52cc4223ac37353128c08a0
Francesco <delta873@gmail.com>
2021-03-29 15:49:08 +0200

    Creati model con relative funzioni di relazioni e controllers

--- app/Author.php
+++ app/Author.php
@@ -6,5 +6,11 @@ use Illuminate\Database\Eloquent\Model;
 
 class Author extends Model
 {
-    //
+    public function detail(){
+        return $this->hasOne('App\AuthorDetail');
+    }
+
+    public function posts(){
+        return $this->hasMany('App\Post');
+    }
 }
---
+++ app/Http/Controllers/AuthorController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+use App\Author;
+
+class AuthorController extends Controller
+{
+    //
+}
