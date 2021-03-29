6fd14dce204ffca64ae09a32aaa2346713973785
Francesco <delta873@gmail.com>
2021-03-29 16:27:39 +0200

    Creati seeds per author e author_details

--- database/seeds/Post.php
+++ database/seeds/Post.php
@@ -1,8 +1,11 @@
 <?php
 
 use Illuminate\Database\Seeder;
-
 use Faker\Generator as Faker;
+use App\Author;
+use App\AuthorDetail;
+
+
 
 class Post extends Seeder
 {
@@ -11,8 +14,18 @@ class Post extends Seeder
      *
      * @return void
      */
-    public function run()
+    public function run(Faker $faker)
     {
+        for($i = 0; $i < 20; $i++){
+            $author = new Author();
+            $author->name = $faker->firstName();
+            $author->surname = $faker->lastName();
+            $author->save();
 
+            $authorDetail = new AuthorDetail();
+            $authorDetail->email_adress = $faker->email();
+            $authorDetail->pic =  'https://picsum.photos/seed/' . rand(0,1000) . '/200/300';
+            $author->detail()->save($authorDetail);
+        }
     }
 }
