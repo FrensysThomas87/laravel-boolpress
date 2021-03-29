31b2273c45be08f5846b004e03f3a651e713087a
Francesco <delta873@gmail.com>
2021-03-29 16:41:37 +0200

    Creata pagina index con tabella authors e author_details

---
+++ resources/views/authors/index.blade.php
@@ -0,0 +1,29 @@
+@extends('authors.templates.base')
+
+@section('title', 'Authors')
+
+@section('index-content')
+<table class="table">
+    <thead class="thead-dark">
+      <tr>
+        <th scope="col">#</th>
+        <th scope="col">Name</th>
+        <th scope="col">Surname</th>
+        <th scope="col">Email Adress</th>
+        <th scope="col">Picture</th>
+      </tr>
+    </thead>
+    <tbody>
+
+      @foreach ( $authors as $author )
+      <tr>
+        <th scope="row">{{$author->id}}</th>
+        <td>{{$author->name}}</td>
+        <td>{{$author->surname}}</td>
+        <td>{{$author->detail->email_adress}}</td>
+        <td><img src="{{$author->detail->pic}}" alt="Picture"></td>
+      </tr>
+      @endforeach
+
+</table>
+@endsection
